4378af84fa123b3e21ee2ad28b6d3281398e7ba5
themaverick27 <aniweshkumar04@gmail.com>
2026-08-28 18:13:33 +0530

    added authentication functionality

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blogs',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -125,6 +127,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 
 # Email
--- blog_main/views.py
+++ blog_main/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog_main.forms import RegistrationForm
 from blogs.models import Category, Blog, About, SocialLink
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 def home(request):
     featured_posts = Blog.objects.filter(is_featured=True, status="Published").order_by('updated_at')
@@ -17,3 +20,45 @@ def home(request):
         'about': about,
     }
     return render(request, 'home.html', context)
+
+def register(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register')
+        #else:
+        #    print(form.error)
+    else:
+        form = RegistrationForm()
+
+    context = {
+        'form': form
+    }
+    return render(request, 'register.html', context)
+
+def login(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
+    else:
+        form = AuthenticationForm()
+            
+
+    form = AuthenticationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'login.html', context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
