ace4de12cbd9cb75c045c73a9e642c6692bff53c
themaverick27 <aniweshkumar04@gmail.com>
2026-08-29 20:52:41 +0530

    added "Add Blog Post" Functionality

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from blogs.views import Category, Blog
 from django.contrib.auth.decorators import login_required
-from .forms import CategoryForm
+from .forms import BlogPostForm, CategoryForm
+from django.template.defaultfilters import slugify
 
 # Create your views here.
 @login_required(login_url="login")
@@ -51,3 +52,33 @@ def delete_category(request, pk):
     category.delete()
 
     return redirect('categories')
+
+def posts(request):
+    posts = Blog.objects.all()
+    context = {
+        'posts':posts,
+    }
+    return render(request, 'dashboard/posts.html', context)
+
+def add_post(request):
+    if request.method == "POST":
+        form = BlogPostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False) # temporarily saving the form, not uploaded to the db yet
+            post.author = request.user
+            post.save() # data is stored to db, unique id (pk) at this point is generated
+
+            title = form.cleaned_data['title']
+            post.slug = slugify(title) + '-'+ str(post.id)
+            post.save()
+            return redirect('posts')
+        else:
+            print('form is invalid')
+            print(form.errors)
+
+
+    form = BlogPostForm()
+    context = {
+        'form':form,
+    }
+    return render(request, 'dashboard/add_post.html', context)
