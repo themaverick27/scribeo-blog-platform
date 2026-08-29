770ef34cd5eef27268660e0f651e8d6121f37cb2
themaverick27 <aniweshkumar04@gmail.com>
2026-08-29 16:02:06 +0530

    Added categories CRUD functionality

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('categories/', views.categories, name='categories'),
+    path('categories/add/', views.add_category, name="add_category"),
+    path('categories/edit/<int:pk>/', views.edit_category, name="edit_category"),
+    path('categories/delete/<int:pk>/', views.delete_category, name="delete_category"),
 ]
--- dashboards/views.py
+++ dashboards/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
 from blogs.views import Category, Blog
 from django.contrib.auth.decorators import login_required
+from .forms import CategoryForm
 
 # Create your views here.
 @login_required(login_url="login")
@@ -16,3 +17,37 @@ def dashboard(request):
 
 def categories(request):
     return render(request, 'dashboard/categories.html')
+
+def add_category(request):
+    if request.method == "POST":
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+
+    form = CategoryForm()
+    context = {
+        'form':form
+    }
+    return render(request, "dashboard/add_category.html", context)
+
+def edit_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    if request.method == 'POST':
+        form = CategoryForm(request.POST, instance=category)
+        if form.is_valid():
+            form.save()
+            return redirect('categories')
+
+    form = CategoryForm(instance=category)
+    context = {
+        'form': form,
+        'category': category
+    }
+    return render(request, 'dashboard/edit_category.html', context)
+
+def delete_category(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+
+    return redirect('categories')
